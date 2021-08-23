c49cc7a6229ee07c0bd7e844c11f53191d9b9a86
Daria Krestina <dariakrestina@Darias-MacBook.local>
2021-08-23 16:31:29 -0400

    Linked Home.jsx to App.js

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,14 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import Place from '../components/Place'
+import { Link } from 'react-router-dom'
 
 function Home(props) {
   const { places } = props
   return (
     places.map((place) => (
-    <>
-        <img alt='tourist attraction pic' src={place.fields.image} />
-        <h2>{place.fields.name}, {place.fields.country}</h2>
+      <>
+        <Link to={`/places/${place.id}`}>
+          <img alt='tourist attraction pic' src={place.fields.image} />
+          <h2>{place.fields.name}, {place.fields.country}</h2>
+        </Link>
     </>
   ))
   )
