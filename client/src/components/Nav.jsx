3ea3d725954edb944f6b72794644ef8eb582e8d9
Daria Krestina <dariakrestina@Darias-MacBook-Pro.local>
2021-08-25 14:37:42 -0400

    Ready for deployment

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import "./Nav.css"
 
 function Nav() {
 
   return (
     <>
-      <Link to="/"><h1 id="top">Places to go</h1></Link>
       <nav>
+        <Link to="/"><h1 id="top">Places to go</h1></Link>
         <Link to="/"><img src="https://i.imgur.com/yypAWmK.png" alt="Home"/></Link>
         <Link to="/new"><img src="https://i.imgur.com/uKjzGW7.png" alt="Add"/></Link>
       </nav>
