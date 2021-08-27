dba0779fbf509c9a1e2996465c8884c125071ed8
Daria Krestina <dariakrestina@Darias-MacBook-Pro.local>
2021-08-26 22:18:46 -0400

    Complete place component css for desktop and tablet

--- client/src/components/Place.jsx
+++ client/src/components/Place.jsx
@@ -13,27 +13,31 @@ function Place(props) {
         <Row>
           <Col><img alt="tourist attraction pic" src={place.fields.image} width="100%"/></Col>
           <Col>
-            <div className="country-name">
-              <h1>{place.fields.name}</h1>
-              <h4>{place.fields.country}</h4>
+            <div className="place-info">
+              <div className="country-name">
+                <h1>{place.fields.name}</h1>
+                <h4>{place.fields.country}</h4>
+              </div>
+              <h3>Languages</h3>
+              <p>{place.fields.languages}</p>
+              <h3>Currency</h3>
+                <p>{place.fields.currency}</p>
             </div>
-            <h3>Languages</h3>
-            <p>{place.fields.languages}</p>
-            <h3>Currency</h3>
-            <p>{place.fields.currency}</p>
           </Col>
         </Row>
         <Row>
           <Col>
-            <h3>Description</h3>
-            <p>{place.fields.description}</p>
-            <h3>Things to do</h3>
-            <p>{place.fields.thingsToDo}</p>
-            <Link to={`/edit/${place.id}`}>
-              <div className="d-grid gap-2">
-                <Button variant="info">Edit</Button>
-              </div>
-            </Link> 
+            <div className="place-description">
+              <h3>Description</h3>
+              <p>{place.fields.description}</p>
+              <h3>Things to do</h3>
+              <p>{place.fields.thingsToDo}</p>
+              <Link to={`/edit/${place.id}`}>
+                <div className="d-grid gap-2 button">
+                  <Button variant="info">Edit</Button>
+                </div>
+              </Link>
+            </div>
           </Col>
         </Row>
       </Container>
